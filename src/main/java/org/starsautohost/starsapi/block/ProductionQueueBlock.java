60917b8496aef9aef7410b6f0598fd1cd47fdd4b
raptor <buckyballreaction@gmail.com>
2020-04-24 07:33:07 -0600

    Decode the ProductionQueueBlock and the ProductionQueueChangeBlock. Mostly.

--- src/main/java/org/starsautohost/starsapi/block/ProductionQueueBlock.java
+++ src/main/java/org/starsautohost/starsapi/block/ProductionQueueBlock.java
@@ -1,15 +1,48 @@
 package org.starsautohost.starsapi.block;
 
+import java.util.Vector;
+
+import org.starsautohost.starsapi.Util;
+
 public class ProductionQueueBlock extends Block {
 
+	public class QueueItem {
+		int itemId;
+		int count;
+		int completePercent;
+		int unknownBits;
+	}
+
+	Vector<QueueItem> queueItems = new Vector<QueueItem>();
+	
+	/**
+	 * Note that this block does not have a planet ID with it like the
+	 * {@link ProductionQueueChangeBlock}. It comes directly after the 
+	 * {@link PartialPlanetBlock} with its ID. 
+	 */
 	public ProductionQueueBlock() {
 		typeId = BlockType.PRODUCTION_QUEUE;
 	}
 
 	@Override
 	public void decode() {
-		// TODO Auto-generated method stub
+		// Every 4 bytes from here on out is queue data
+		for(int i = 0; i <= decryptedData.length - 4; i+=4) {
+			int chunk1 = Util.read16(decryptedData, i); // Read 2
+			int chunk2 = Util.read16(decryptedData, i+2); // Read 2
+			
+			// Build up a QueueItem
+			QueueItem item = new QueueItem();
 
+			item.itemId 	= chunk1 >> 10;   // Top 6 - but only uses 4?
+			item.count 	  	= chunk1 & 0x3FF; // Bottom 10 bits
+
+			item.completePercent = chunk2 >> 4;  // Top 12 bits
+			item.unknownBits     = chunk2 & 0xF; // Bottom 4 bits
+			
+			// Add to list
+			queueItems.add(item);
+		}
 	}
 
 	@Override
@@ -17,5 +50,20 @@ public class ProductionQueueBlock extends Block {
 		// TODO Auto-generated method stub
 		
 	}
-
+	
+	@Override
+	public String toString() {
+		String s = "ProductionQueueBlock: ";
+		
+		s += "Queue items (itemId, count, complete %, unknown):\n";
+		
+		for(QueueItem queueItem: queueItems) {
+			s += queueItem.itemId + "\t";
+			s += queueItem.count + "\t";
+			s += queueItem.completePercent + "\t";	
+			s += queueItem.unknownBits + "\n";
+		}
+		
+		return s;
+	}
 }
--- src/main/java/org/starsautohost/starsapi/block/ProductionQueueChangeBlock.java
+++ src/main/java/org/starsautohost/starsapi/block/ProductionQueueChangeBlock.java
@@ -1,15 +1,46 @@
 package org.starsautohost.starsapi.block;
 
-public class ProductionQueueChangeBlock extends Block {
+import java.util.Vector;
 
+import org.starsautohost.starsapi.Util;
+
+public class ProductionQueueChangeBlock extends Block {
+	
+	public class QueueItem {
+		int itemId;
+		int count;
+		int completePercent;
+		int unknownBits;
+	}
+
+	int planetId;
+	Vector<QueueItem> queueItems = new Vector<QueueItem>();
+	
 	public ProductionQueueChangeBlock() {
 		typeId = BlockType.PRODUCTION_QUEUE_CHANGE;
 	}
 
 	@Override
 	public void decode() {
-		// TODO Auto-generated method stub
+		planetId = Util.read16(decryptedData, 0);
+		
+		// Every 4 bytes from here on out is queue data
+		for(int i = 2; i <= decryptedData.length - 4; i+=4) {
+			int chunk1 = Util.read16(decryptedData, i); // Read 2
+			int chunk2 = Util.read16(decryptedData, i+2); // Read 2
+			
+			// Build up a QueueItem
+			QueueItem item = new QueueItem();
 
+			item.itemId 	= chunk1 >> 10;   // Top 6 - but only uses 4?
+			item.count 	  	= chunk1 & 0x3FF; // Bottom 10 bits
+
+			item.completePercent = chunk2 >> 4;  // Top 12 bits
+			item.unknownBits     = chunk2 & 0xF; // Bottom 4 bits
+			
+			// Add to list
+			queueItems.add(item);
+		}
 	}
 
 	@Override
@@ -17,5 +48,21 @@ public class ProductionQueueChangeBlock extends Block {
 		// TODO Auto-generated method stub
 		
 	}
-
+	
+	@Override
+	public String toString() {
+		String s = "ProductionQueueChangeBlock: ";
+		
+		s += "Planet ID: " + planetId + "\n";
+		s += "Queue items (itemId, count, complete %, unknown):\n";
+		
+		for(QueueItem queueItem: queueItems) {
+			s += queueItem.itemId + "\t";
+			s += queueItem.count + "\t";
+			s += queueItem.completePercent + "\t";	
+			s += queueItem.unknownBits + "\n";
+		}
+		
+		return s;
+	}
 }
